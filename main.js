17b34e4add0736383e3bb49e3b793e6bd747b488
lgoliveira <lucas.guimaraes.lc@gmail.com>
2024-09-07 15:14:22 -0300

    Add Electron

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron/main');
+const path = require('node:path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (process.env.ELECTRON_DEV) {
+    win.loadURL('http://localhost:4200');
+    return win.webContents.openDevTools();
+  }
+
+  win.loadFile('dist/iluminando-vidas/browser/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
